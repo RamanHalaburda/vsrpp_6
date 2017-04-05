708f0522f5c3efb36b9815a0da4f3520c9a2471d
RomanHalaburda <mtbrider94@gmail.com>
2017-04-05 16:08:51 +0300

    initialize commit

---
+++ vsrpp_6/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vsrpp_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+            1. На перегрузку операторов. Описать класс комплексных чисел.
+            Реализовать операцию сложения, умножения, вычитания, проверку на
+            равенство двух комплексных чисел. Переопределить метод ToString() для
+            комплексного числа. Протестировать на простом примере.
+        */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
